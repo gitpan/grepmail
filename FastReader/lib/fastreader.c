e3079c9d1fab51a6650602dcc3ddcf64f144193e
David Coppit <david@coppit.org>
2001-01-17 02:52:05 +0000

    Import of DCOPPIT/grepmail-4.45 from CPAN.
    gitpan-cpan-distribution: grepmail
    gitpan-cpan-version:      4.45
    gitpan-cpan-path:         DCOPPIT/grepmail-4.45.tar.gz
    gitpan-cpan-author:       DCOPPIT
    gitpan-cpan-maturity:     released

--- FastReader/lib/fastreader.c
+++ FastReader/lib/fastreader.c
@@ -1,174 +1,177 @@
 #include <stdio.h>
 #include <string.h>
-#include "fastreader.h"
 
 #define DEBUG 0
 
 char *_find_next_header(char* buffer);
-int _ends_with_include(char *email);
-
-static int BUFFER_SIZE_INCREMENT=1000000;
-static int BUFFER_SIZE=0;
-
-/* The current line count */
-static long LINE = 1;
-
-/* The last line read */
-static char LAST_LINE[256];
+int _ends_with_include(char *email_boundary);
 
 /* Storage for the email */
 static char *EMAIL_BUFFER = NULL;
 
+/* Amount to increase the buffer by when it fills up */
+static unsigned int EMAIL_BUFFER_SIZE_INCREMENT=1000000;
+
+/* The current size of the buffer */
+static unsigned int EMAIL_BUFFER_SIZE=0;
+
+/* The current line count */
+static long LINE_NUMBER = 0;
+
+/* The last line read */
+static char LAST_LINE[1025];
+
+/* The length of the email */
 static unsigned int LENGTH_OF_EMAIL = 0;
 
+/* The file pointer */
+static FILE *FILE_HANDLE;
 
-int read_email(FILE *file_handle,char **email,long *email_line)
+/**************************************************************************/
+
+/* Read one email from the file, setting *email to point to it, and
+ * email_line to the line in the file on which it appears */
+
+int read_email(char **email,long *email_line)
 {
-  char next_line[256];
+  char *start_of_new_line;
+  unsigned int number_characters_read;
 
-  if (feof(file_handle))
+  /* Can't read from the end of the file */
+  if (feof(FILE_HANDLE))
   {
 #if DEBUG==1 || DEBUG==2 || DEBUG==3
-    fprintf(stderr,"eof, and no more buffered data\n");
+    fprintf(stderr,"eof\n");
 #endif
-    reset_line();
     return 0;
   }
 
-  /* Allocate the memory if it hasn't been already. */
-  if (LINE == 0)
-  {
-    fgets(LAST_LINE,255,file_handle);
-    LINE = 1;
-  }
+  /* If this is the first email, pre-load the email buffer. */
+  if (LINE_NUMBER == 0)
+    fgets(EMAIL_BUFFER,1024,FILE_HANDLE);
+  else
+    strcpy(EMAIL_BUFFER,LAST_LINE);
 
-  if (EMAIL_BUFFER == NULL)
-  {
-    BUFFER_SIZE = BUFFER_SIZE_INCREMENT;
-    EMAIL_BUFFER = (char *)malloc((BUFFER_SIZE)*sizeof(char));
-  }
+  LENGTH_OF_EMAIL = strlen(EMAIL_BUFFER);
+  LINE_NUMBER++;
 
-  LENGTH_OF_EMAIL = 0;
-  EMAIL_BUFFER[LENGTH_OF_EMAIL] = '\0';
+  /* Set the line number to return */
+  *email_line = LINE_NUMBER;
 
-  strcpy(EMAIL_BUFFER,LAST_LINE);
-  LENGTH_OF_EMAIL += strlen(LAST_LINE);
-
-  *email = EMAIL_BUFFER;
-  *email_line = LINE;
-
+  /* Keep reading lines until we hit the next email or EOF. At this
+   * point we have 1 line of the email in the buffer. */
   while (1)
   {
 #if DEBUG==3
-  fprintf(stderr,".");
+    fprintf(stderr,".");
 #endif
-    fgets(next_line,255,file_handle);
 
-    if (feof(file_handle))
+    /* Extend the size of the buffer if necessary to accomodate 1 more
+     * line. */
+    if (EMAIL_BUFFER_SIZE < LENGTH_OF_EMAIL + 1024)
     {
 #if DEBUG==1
-  fprintf(stderr,"hit eof\n");
+      fprintf(stderr,"extending\n");
 #endif
-      reset_line();
+      EMAIL_BUFFER_SIZE += EMAIL_BUFFER_SIZE_INCREMENT;
+      EMAIL_BUFFER = (char *)realloc(EMAIL_BUFFER,EMAIL_BUFFER_SIZE*sizeof(char));
+    }
+
+    /* Read a line from the file and store it at the end of the email
+     * buffer. We will move it to LAST_LINE if it turns out to
+     * be the start of the next email. */
+    start_of_new_line = EMAIL_BUFFER+LENGTH_OF_EMAIL;
+    fgets(start_of_new_line,1024,FILE_HANDLE);
+    number_characters_read = strlen(start_of_new_line);
+    LENGTH_OF_EMAIL += number_characters_read;
+
+    /* If we hit the end of the file, return the email */
+    if (feof(FILE_HANDLE))
+    {
+#if DEBUG==1
+      fprintf(stderr,"hit eof\n");
+#endif
+
+      *email = EMAIL_BUFFER;
       return 1;
     }
 
-    LINE++;
+    if (start_of_new_line[number_characters_read-1] == '\n')
+      LINE_NUMBER++;
 
-    if(next_line[0] == 'F' && next_line[1] == 'r' && next_line[2] == 'o' &&
-       next_line[3] == 'm' && next_line[4] == ' ')
+    /* See if the line is the start of a new email. Try to avoid
+     * unescaped "From my mother" lines by checking that the last 4
+     * characters are numbers. */
+    if(start_of_new_line[-1] == '\n' && start_of_new_line[0] == 'F' &&
+       start_of_new_line[1]  == 'r'  && start_of_new_line[2] == 'o' &&
+       start_of_new_line[3]  == 'm'  && start_of_new_line[4] == ' ' &&
+       isdigit(start_of_new_line[number_characters_read-2]) &&
+       isdigit(start_of_new_line[number_characters_read-3]) &&
+       isdigit(start_of_new_line[number_characters_read-4]) &&
+       isdigit(start_of_new_line[number_characters_read-5]))
     {
-      if(_ends_with_include(EMAIL_BUFFER))
+      /* If the email doesn't end with an included message declaration,
+       * then the From line must be the start of a new email. */
+      if(!_ends_with_include(start_of_new_line))
       {
 #if DEBUG==1
-  fprintf(stderr,"found include\n");
+  fprintf(stderr,"found next email\n");
 #endif
-        /* Extend the size of the buffer if necessary */
-        if (BUFFER_SIZE < LENGTH_OF_EMAIL + 255)
-        {
-#if DEBUG==1
-  fprintf(stderr,"extending\n");
-#endif
-          BUFFER_SIZE += BUFFER_SIZE_INCREMENT;
-          EMAIL_BUFFER = (char *)realloc(EMAIL_BUFFER,BUFFER_SIZE*sizeof(char));
-          *email = EMAIL_BUFFER;
-        }
+        strncpy(LAST_LINE,start_of_new_line,1024);
+        LENGTH_OF_EMAIL -= strlen(start_of_new_line);
+        *start_of_new_line = '\0';
+        LINE_NUMBER--;
 
-        strcpy(EMAIL_BUFFER+LENGTH_OF_EMAIL,next_line);
-        LENGTH_OF_EMAIL += strlen(next_line);
-      }
-      else
-      {
-#if DEBUG==1
-  fprintf(stderr,"found another email\n");
-#endif
-        strcpy(LAST_LINE,next_line);
+        *email = EMAIL_BUFFER;
         return 1;
       }
-    }
-    else
-    {
-      /* Extend the size of the buffer if necessary */
-      if (BUFFER_SIZE < LENGTH_OF_EMAIL + 255)
-      {
-#if DEBUG==1
-  fprintf(stderr,"extending\n");
-#endif
-        BUFFER_SIZE += BUFFER_SIZE_INCREMENT;
-        EMAIL_BUFFER = (char *)realloc(EMAIL_BUFFER,BUFFER_SIZE*sizeof(char));
-        *email = EMAIL_BUFFER;
-      }
-
-      strcpy(EMAIL_BUFFER+LENGTH_OF_EMAIL,next_line);
-      LENGTH_OF_EMAIL += strlen(next_line);
     }
   }
 }
 
-void reset_line()
+void reset_file(FILE *infile)
 {
 #if DEBUG==1 || DEBUG==2 || DEBUG==3
   fprintf(stderr,"resetting line number\n");
 #endif
-  LINE = 0;
+
+  /* Initialize the email buffer if it has not been initialized already. */
+  if (EMAIL_BUFFER == NULL)
+  {
+    EMAIL_BUFFER_SIZE = EMAIL_BUFFER_SIZE_INCREMENT;
+    EMAIL_BUFFER = (char *)malloc((EMAIL_BUFFER_SIZE)*sizeof(char));
+  }
+
+  FILE_HANDLE = infile;
+  LINE_NUMBER = 0;
 }
 
-int _ends_with_include(char *email)
+int _ends_with_include(char *email_boundary)
 {
   char *location;
   int newlines;
 
-  location = strstr(email,"\n----- Begin Included Message -----\n");
+  location = strstr(email_boundary-255,"\n----- Begin Included Message -----\n");
 
   if (location == 0)
-  {
     return 0;
-  }
 
   /* Find the last newline after the begin included message */
   newlines = strspn(location + 35,"\n");
 
-  if (location[35+newlines] == '\0')
-  {
+  if(location+35+newlines == email_boundary)
     return 1;
-  }
 
-  location = strstr(email,"\n-----Original Message-----\n");
+  location = strstr(email_boundary-255,"\n-----Original Message-----\n");
 
   if (location == 0)
-  {
     return 0;
-  }
 
   /* Find the last newline after the begin included message */
   newlines = strspn(location + 27,"\n");
 
-  if (location[27+newlines] == '\0')
-  {
+  if(location+27+newlines == email_boundary)
     return 1;
-  }
   else
-  {
     return 0;
-  }
 }
